eebf1ad76785cbd269878d80be296abd914f94ee
Melissa <melissa.gonzalez@correounivalle.edu.co>
2021-09-19 14:46:43 -0500

    functional form

--- cliente/src/Registroinfo.js
+++ cliente/src/Registroinfo.js
@@ -1,36 +1,69 @@
-import React from "react";
-import { Question } from "./Question.js";
+import React, { useState } from "react";
 
 export const Registroinfo = () => {
+  const [values, setValues] = useState({
+    food_fav: "",
+    gen_fav: "",
+  });
+
+  const options_values = [
+    {
+      id: 0,
+      name: "Comida rápida",
+    },
+    {
+      id: 1,
+      name: "Comida típica colombiana",
+    },
+    {
+      id: 2,
+      name: "Comida asiática",
+    },
+    {
+      id: 3,
+      name: "Repostería",
+    },
+    {
+      id: 4,
+      name: "Otro",
+    },
+  ];
+
+  const handleChange = (e) => {
+    setValues({
+      ...values,
+      food_fav: e.target.value,
+    });
+  };
+
+  // const handleChange2 = (e) => {
+  //   setValues({
+  //     ...values,
+  //     gen_fav: e.target.value,
+  //   });
+  // };
+
   return (
     <div className="formdb__box-containter">
       <h3 className="auth__title">Envio INFO</h3>
       <form>
-        <Question
-          question="¿Qué tipo de comida prefiere?"
-          options_values={[
-            {
-              id: 0,
-              name: "Comida rápida",
-            },
-            {
-              id: 1,
-              name: "Comida típica colombiana",
-            },
-            {
-              id: 2,
-              name: "Comida asiática",
-            },
-            {
-              id: 3,
-              name: "Repostería",
-            },
-            {
-              id: 4,
-              name: "Otro",
-            },
-          ]}
-        ></Question>
+        <div>
+          <h3>{"¿Qué tipo de comida prefiere?"}</h3>
+          {options_values.map((option, key) => (
+            <div key={key}>
+              <label>
+                <input
+                  type="checkbox"
+                  value={option.name}
+                  checked={values.food_fav === option.name}
+                  onChange={handleChange}
+                />
+                {option.name}
+              </label>
+            </div>
+          ))}
+          {console.log(values)}
+        </div>
       </form>
       <button className="btn btn-primary" type="button">
         Enviar
